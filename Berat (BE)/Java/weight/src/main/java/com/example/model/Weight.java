e90528dbab1e4344f7a933d26577bb9c1d3fa0b1
Saul Sayers <saulsayers@gmail.com>
2023-07-05 13:49:27 +0700

    feat update java BE code

--- Berat (BE)/Java/weight/src/main/java/com/example/model/Weight.java
+++ Berat (BE)/Java/weight/src/main/java/com/example/model/Weight.java
@@ -1,23 +1,61 @@
 package com.example.model;
 
-import lombok.Data;
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name = "weights")
-@Data
+@Table(name = "weight")
 public class Weight {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "date")
+    @Column(name = "tanggal")
     private Date date;
 
-    @Column(name = "max_weight")
+    @Column(name = "berat_max")
     private double maxWeight;
 
-    @Column(name = "min_weight")
+    @Column(name = "berat_min")
     private double minWeight;
+
+    // Getter for id
+    public Long getId() {
+        return id;
+    }
+
+    // Setter for id
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter for date
+    public Date getDate() {
+        return date;
+    }
+
+    // Setter for date
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    // Getter for maxWeight
+    public double getMaxWeight() {
+        return maxWeight;
+    }
+
+    // Setter for maxWeight
+    public void setMaxWeight(double maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    // Getter for minWeight
+    public double getMinWeight() {
+        return minWeight;
+    }
+
+    // Setter for minWeight
+    public void setMinWeight(double minWeight) {
+        this.minWeight = minWeight;
+    }
 }
